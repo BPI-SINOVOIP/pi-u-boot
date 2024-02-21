006d75dbf8bff1084712a092bd31454a4a61ea5b
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2024-02-21 23:45:25 +0800

    add get_chip_id

--- arch/arm/mach-synaptics/init.c
+++ arch/arm/mach-synaptics/init.c
@@ -1,12 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2016~2023 Synaptics Incorporated. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 or
+ * later as published by the Free Software Foundation.
+ *
+ * INFORMATION CONTAINED IN THIS DOCUMENT IS PROVIDED "AS-IS," AND
+ * SYNAPTICS EXPRESSLY DISCLAIMS ALL EXPRESS AND IMPLIED WARRANTIES,
+ * INCLUDING ANY IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE, AND ANY WARRANTIES OF NON-INFRINGEMENT OF ANY
+ * INTELLECTUAL PROPERTY RIGHTS. IN NO EVENT SHALL SYNAPTICS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, PUNITIVE, OR
+ * CONSEQUENTIAL DAMAGES ARISING OUT OF OR IN CONNECTION WITH THE USE
+ * OF THE INFORMATION CONTAINED IN THIS DOCUMENT, HOWEVER CAUSED AND
+ * BASED ON ANY THEORY OF LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, AND EVEN IF SYNAPTICS WAS
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. IF A TRIBUNAL OF
+ * COMPETENT JURISDICTION DOES NOT PERMIT THE DISCLAIMER OF DIRECT
+ * DAMAGES OR ANY OTHER DAMAGES, SYNAPTICS' TOTAL CUMULATIVE LIABILITY
+ * TO ANY PARTY SHALL NOT EXCEED ONE HUNDRED U.S. DOLLARS.
+ */
+
 #include <common.h>
 #include <console.h>
+#include <asm/io.h>
 #include <dm.h>
 #include <flash_ts.h>
 #include <misc.h>
 #include <usb.h>
 #include <mmc.h>
+#include "global.h"
+#include "Galois_memmap.h"
 
 DECLARE_GLOBAL_DATA_PTR;
+
+#define BL_ARG_MAGIC 0xdeadbeaf
+struct bl_boot_arg {
+	unsigned long param[8];		// the first param is set to the address of bl_boot_arg.
+	unsigned int flash_param[3];	// the original param
+	unsigned int leakage;		// leakage id
+	unsigned int chip_id[2];	// unique chip id
+	unsigned int soc_tsen_id;	// soc tsen_id
+	unsigned int cpu_tsen_id;	// cpu tsen_id
+	unsigned int chip_revision;	// chip revision: Z1/A0/A1/A2
+};
+
+static unsigned long param2_save = 0xFFFFFFFF;
 
 typedef struct boot_info {
 	char mac_addr[32];
@@ -172,4 +212,56 @@ int arch_misc_init(void)
 		return ret;
 #endif
 	return ret;
+}
+
+void save_boot_params(unsigned long param1, unsigned long param2, unsigned long param3)
+{
+	if (param1 == BL_ARG_MAGIC)
+		param2_save = param2;
+
+	save_boot_params_ret();
+}
+
+int get_chip_type(void)
+{
+	if (param2_save != 0xFFFFFFFF)
+		return ((struct bl_boot_arg *)param2_save)->chip_revision >> 16;
+
+	return -1;
+}
+
+unsigned long long get_chip_id(void)
+{
+	unsigned long long system_serial_low = 0, system_serial_high = 0;
+
+	if (param2_save != 0xFFFFFFFF) {
+		system_serial_low = ((struct bl_boot_arg *)param2_save)->chip_id[0];
+		system_serial_high = ((struct bl_boot_arg *)param2_save)->chip_id[1];
+		return system_serial_low << 32 | system_serial_high;
+	}
+
+	return 0;
+}
+
+int get_chip_rev(void)
+{
+	int rev_major = -1;
+	int rev_minor = -1;
+
+	if (param2_save != 0xFFFFFFFF) {
+		rev_major = readl(MEMMAP_CHIP_CTRL_REG_BASE + RA_Gbl_ProductId_ext);
+		rev_minor = ((struct bl_boot_arg *)param2_save)->chip_revision & 0xFFFF;
+
+		return rev_major + rev_minor;
+	}
+
+	return -1;
+}
+
+int get_leakage(void)
+{
+	if (param2_save != 0xFFFFFFFF)
+		return ((struct bl_boot_arg *)param2_save)->leakage;
+
+	return -1;
 }
